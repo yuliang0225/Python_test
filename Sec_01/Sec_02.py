b450694572d0485469a72c8ebf4ac03ec9243fb6
Smuch_Linux_home <smuchpaker@yahoo.co,jp>
2017-12-17 21:10:38 +0900

    Add Sect02

--- Sec_01/Sec_02.py
+++ Sec_01/Sec_02.py
@@ -5,5 +5,21 @@ Created on Sun Dec 17 20:48:51 2017
 
 @author: smuch
 """
-# Section 2
+# Section 2 Supervised Learning
+#%% Import necessary toolbox
+import mglearn
+import matplotlib.pyplot as plt
+from IPython.display import display
+import pandas as pd
+import numpy as np
+#%% Sec 2.3
+# generate dataset
+X, y = mglearn.datasets.make_forge()
+# plot dataset
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.legend(["Class 0", "Class 1"], loc=4)
+plt.xlabel("First feature")
+plt.ylabel("Second feature")
+print("X.shape: {}".format(X.shape))
 #%%
+
