3412a22f836ee53ea0f0285696cd1488fa344f84
Smuch_Linux_home <smuchpaker@yahoo.co,jp>
2017-12-17 19:34:02 +0900

    TEST

--- Sec_01/Sec_01.py
+++ Sec_01/Sec_01.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
+Smuch!
 This is a temporary script file.
 """
 #%% Test NP
@@ -16,6 +16,13 @@ print ("NumPy array:\n{}".format(eye))
 sparse_martix = sparse.csr_matrix(eye)
 print("\nSciPy sparse.csr martix:\n{}".format(sparse_martix))
 
+data = np.ones(4)
+row_indices = np.arange(4)
+col_indices = np.arange(4)
+eye_coo = sparse.coo_matrix((data,(row_indices,col_indices)))
+print("COO :\n {}".format(eye_coo))
 #%% Test Matplotlib 
+
+
 
 #%%
