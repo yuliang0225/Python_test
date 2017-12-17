d1854687df8b2c86c0ee09b5b2a71d88b12d0772
Smuch_Linux_home <smuchpaker@yahoo.co,jp>
2017-12-17 21:33:41 +0900

    Add Sect02

--- Sec_01/Sec_02.py
+++ Sec_01/Sec_02.py
@@ -22,4 +22,30 @@ plt.xlabel("First feature")
 plt.ylabel("Second feature")
 print("X.shape: {}".format(X.shape))
 #%%
+X, y = mglearn.datasets.make_wave(n_samples=40)
+plt.plot(X, y, 'o')
+plt.ylim(-3, 3)
+plt.xlabel("Feature")
+plt.ylabel("Target")
+#%% Cancer data
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+print("cancer.keys(): {}".format(cancer.keys()))
+print("Shape of cancer data: {}".format(cancer.data.shape)) # 569*30
+print("Sample counts per class:\n{}".format(
+      {n: v for n, v in zip(cancer.target_names, np.bincount(cancer.target))})) #357 : 212
+print("Feature names:\n{}".format(cancer.feature_names)) # Features
+#%% Boston data
+from sklearn.datasets import load_boston
+boston = load_boston()
+print("Data shape: {}".format(boston.data.shape)) # 506*13
+#%% KNN
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+#%%
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+
+
+
+
 
