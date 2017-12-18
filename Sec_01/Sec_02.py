cc608a387db31bc29b598d089760d661f9636159
Smuch_Linux_home <smuchpaker@yahoo.co,jp>
2017-12-18 21:15:33 +0900

    NO

--- Sec_01/Sec_02.py
+++ Sec_01/Sec_02.py
@@ -39,10 +39,119 @@ print("Feature names:\n{}".format(cancer.feature_names)) # Features
 from sklearn.datasets import load_boston
 boston = load_boston()
 print("Data shape: {}".format(boston.data.shape)) # 506*13
-#%% KNN
+#%% KNN Know the neighbours number
 mglearn.plots.plot_knn_classification(n_neighbors=1)
 #%%
-mglearn.plots.plot_knn_classification(n_neighbors=3)
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+#%%
+from sklearn.model_selection import train_test_split
+X, y = mglearn.datasets.make_forge()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(X_train, y_train)
+print("Test set predictions: {}".format(clf.predict(X_test)))
+print("Test set accuracy: {:.2f}".format(clf.score(X_test, y_test)))
+#%% Different neighbors
+fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    # the fit method returns the object self, so we can instantiate
+    # and fit in one line:
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{} neighbor(s)".format(n_neighbors))
+    ax.set_xlabel("feature 0")
+    ax.set_ylabel("feature 1")
+axes[0].legend(loc=3)
+#%%
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=66)
+
+training_accuracy = []
+test_accuracy = []
+# try n_neighbors from 1 to 10
+neighbors_settings = range(1, 11)
+
+for n_neighbors in neighbors_settings:
+    # build the model
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(clf.score(X_train, y_train))
+    # record generalization accuracy
+    test_accuracy.append(clf.score(X_test, y_test))
+    
+plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
+plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_neighbors")
+plt.legend()
+#%% KNN regression
+mglearn.plots.plot_knn_regression(n_neighbors=2)
+#%%
+from sklearn.neighbors import KNeighborsRegressor
+X, y = mglearn.datasets.make_wave(n_samples=40)
+# split the wave dataset into a training and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+# instantiate the model and set the number of neighbors to consider to 3:
+reg = KNeighborsRegressor(n_neighbors=3)
+# fit the model using the training data and training targets:
+reg.fit(X_train, y_train)
+print("Test set predictions:\n{}".format(reg.predict(X_test)))
+print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
+#%% Analyzing KNeighborsRegressor
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+# create 1,000 data points, evenly spaced between -3 and 3
+line = np.linspace(-3, 3, 1000).reshape(-1, 1)
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    # make predictions using 1, 3, or 9 neighbors
+    reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+    reg.fit(X_train, y_train)
+    ax.plot(line, reg.predict(line))
+    ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+    ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+
+    ax.set_title(
+        "{} neighbor(s)\n train score: {:.2f} test score: {:.2f}".format(
+            n_neighbors, reg.score(X_train, y_train),
+            reg.score(X_test, y_test)))
+    ax.set_xlabel("Feature")
+    ax.set_ylabel("Target")
+axes[0].legend(["Model predictions", "Training data/target",
+                "Test data/target"], loc="best")
+#%%
+mglearn.plots.plot_linear_regression_wave()
+#%% Linear Regression aka Ordinary Least Squares
+from sklearn.linear_model import LinearRegression
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+lr = LinearRegression().fit(X_train, y_train)
+
+print("lr.coef_: {}".format(lr.coef_))
+print("lr.intercept_: {}".format(lr.intercept_))
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+X, y = mglearn.datasets.load_extended_boston()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
+#%% Ridge regression
+from sklearn.linear_model import Ridge
+
+ridge = Ridge().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))
 
 
 
